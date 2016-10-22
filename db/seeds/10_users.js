feeb081c69ac1f98cb818586931810c80341ff2d
Alex <alexpdraper@gmail.com>
2016-10-22 02:48:46 +0000

    Added read messages seed

--- db/seeds/10_users.js
+++ db/seeds/10_users.js
@@ -6,21 +6,30 @@ function generatePassHash(password) {
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex('read_messages').del()
+    .then(function() {
+      return knex('messages').del();
+    })
+    .then(function() {
+      return knex('users').del();
+    })
     .then(function () {
       return Promise.all([
         // Inserts seed entries
         knex('users').insert({
+          id: 1,
           username: 'devin',
           email: 'devin.gir@gmail.com',
           password_digest: generatePassHash('secret')
         }),
         knex('users').insert({
+          id: 2,
           username: 'john',
           email: 'jchow417@gmail.com',
           password_digest: generatePassHash('secret')
         }),
         knex('users').insert({
+          id: 3,
           username: 'alex',
           email: 'alex@alexanderdraper.com',
           password_digest: generatePassHash('secret')
--- db/seeds/20_messages.js
+++ db/seeds/20_messages.js
@@ -4,25 +4,31 @@ exports.seed = function(knex, Promise) {
 
   // Deletes ALL existing entries
   return knex('messages').del()
-    .then(function () {
+    .then(function() {
+      return knex.select('id').from('users');
+    })
+    .then(function (rows) {
       return Promise.all([
         // Inserts seed entries
         knex('messages').insert({
+          id: 1,
           title: 'A Message From Devin',
           content: 'My name is Devin. This is a message. Hopefully it is in Vancouver.',
-          user_id: knex.select('id').from('users').where({ username: 'devin' }),
+          user_id: rows[0].id,
           location: st.geomFromText('Point(-123.1207 49.2827)', 4326)
         }),
         knex('messages').insert({
+          id: 2,
           title: 'A Message From John',
           content: 'My name is John. This is a message. Hopefully it is good.',
-          user_id: knex.select('id').from('users').where({ username: 'john' }),
+          user_id: rows[1].id,
           location: st.geomFromText('Point(-123.1217 49.283)', 4326)
         }),
         knex('messages').insert({
+          id: 3,
           title: 'A Message From Alex',
           content: 'My name is Alex. This is a message. Hopefully it is messageful.',
-          user_id: knex.select('id').from('users').where({ username: 'alex' }),
+          user_id: rows[2].id,
           location: st.geomFromText('Point(-123.1237 49.2917)', 4326)
         })
       ]);
