9e2f84156162fe736bacac8dff549855f36f1d09
John Chow <JChow417@gmail.com>
2016-10-23 01:59:17 +0000

    feature view geo messages from map complete and displays messages in profile page from mock data from server side

--- public/drop-messages-stub.js
+++ public/drop-messages-stub.js
@@ -2,25 +2,25 @@
 function dropMessagesStub( centralLatLng, map ){
   var curPosn, marker;
 
-  var messages = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut odio auctor, maximus ipsum vitae, sodales libero. Sed elementum lorem vel tortor pharetra imperdiet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis porta enim sed risus lacinia varius. Quisque rhoncus aliquam risus vel iaculis.",
+  var messages = ["1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut odio auctor, maximus ipsum vitae, sodales libero. Sed elementum lorem vel tortor pharetra imperdiet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis porta enim sed risus lacinia varius. Quisque rhoncus aliquam risus vel iaculis.",
 
-  "Cras maximus porttitor neque sit amet dapibus. In porta mauris ut sollicitudin pharetra. Ut vitae feugiat purus. Curabitur sagittis molestie nunc. Curabitur vel mollis purus. Vestibulum eu justo tortor. Morbi ultricies nunc eget porta dictum. Pellentesque in interdum metus. Sed consectetur lectus sit amet commodo rutrum.",
+  "2 Cras maximus porttitor neque sit amet dapibus. In porta mauris ut sollicitudin pharetra. Ut vitae feugiat purus. Curabitur sagittis molestie nunc. Curabitur vel mollis purus. Vestibulum eu justo tortor. Morbi ultricies nunc eget porta dictum. Pellentesque in interdum metus. Sed consectetur lectus sit amet commodo rutrum.",
 
-  "Donec finibus maximus tellus eleifend tristique. Maecenas ultrices metus urna, sit amet cursus lorem varius nec. Phasellus malesuada tortor nec sem consectetur, a posuere urna bibendum. Donec et sapien egestas, vestibulum magna vehicula, aliquet enim. Nunc a urna id quam egestas venenatis id pharetra eros. In et malesuada mi.",
+  "3 Donec finibus maximus tellus eleifend tristique. Maecenas ultrices metus urna, sit amet cursus lorem varius nec. Phasellus malesuada tortor nec sem consectetur, a posuere urna bibendum. Donec et sapien egestas, vestibulum magna vehicula, aliquet enim. Nunc a urna id quam egestas venenatis id pharetra eros. In et malesuada mi.",
 
-  "Praesent non eros turpis. Morbi fringilla consequat nulla, at malesuada nisl mattis consequat. Cras et blandit mi, eu faucibus ex. Nullam a lectus vitae lacus feugiat fermentum. Vivamus efficitur nisl quis dui viverra, elementum suscipit odio congue. In hac habitasse platea dictumst. Aliquam feugiat volutpat erat, vel maximus nisi pharetra vel.",
+  "4 Praesent non eros turpis. Morbi fringilla consequat nulla, at malesuada nisl mattis consequat. Cras et blandit mi, eu faucibus ex. Nullam a lectus vitae lacus feugiat fermentum. Vivamus efficitur nisl quis dui viverra, elementum suscipit odio congue. In hac habitasse platea dictumst. Aliquam feugiat volutpat erat, vel maximus nisi pharetra vel.",
 
-  "Etiam luctus malesuada risus sed ultricies. Donec egestas leo at est molestie fermentum. Sed interdum enim nulla, in tempor elit placerat malesuada. Morbi dictum nibh in odio tincidunt mollis. Curabitur eget tempor neque. Ut ut quam enim.",
+  "5 Etiam luctus malesuada risus sed ultricies. Donec egestas leo at est molestie fermentum. Sed interdum enim nulla, in tempor elit placerat malesuada. Morbi dictum nibh in odio tincidunt mollis. Curabitur eget tempor neque. Ut ut quam enim.",
 
-"Curabitur nisl magna, mattis in nunc id, consequat sagittis ligula. Sed in elit id leo fermentum suscipit at eu nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Morbi placerat enim id dapibus placerat. Praesent suscipit magna velit.",
+"6 Curabitur nisl magna, mattis in nunc id, consequat sagittis ligula. Sed in elit id leo fermentum suscipit at eu nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Morbi placerat enim id dapibus placerat. Praesent suscipit magna velit.",
 
-"Nullam nec ligula a risus ultricies sodales ut id magna. Suspendisse potenti. Praesent ut mi viverra, viverra enim vitae, varius felis. Nam malesuada lacus mi, non mollis urna pretium et. Praesent a risus tempus, interdum neque nec, bibendum nulla.",
+"7 Nullam nec ligula a risus ultricies sodales ut id magna. Suspendisse potenti. Praesent ut mi viverra, viverra enim vitae, varius felis. Nam malesuada lacus mi, non mollis urna pretium et. Praesent a risus tempus, interdum neque nec, bibendum nulla.",
 
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus ligula quis scelerisque tincidunt. Nunc at neque at leo commodo tempor quis ac leo. Proin dapibus posuere mi, id accumsan eros interdum id. Aliquam finibus ut augue in eleifend. Donec gravida convallis suscipit.",
+"8 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus ligula quis scelerisque tincidunt. Nunc at neque at leo commodo tempor quis ac leo. Proin dapibus posuere mi, id accumsan eros interdum id. Aliquam finibus ut augue in eleifend. Donec gravida convallis suscipit.",
 
- "Quisque accumsan elementum neque sit amet efficitur. Duis et neque ut tortor bibendum consequat vitae ut ante. Fusce eu vehicula velit, eu eleifend lorem. Aenean ullamcorper accumsan blandit. Fusce ultricies nulla ipsum, malesuada lacinia purus lacinia eu.",
+ "9 Quisque accumsan elementum neque sit amet efficitur. Duis et neque ut tortor bibendum consequat vitae ut ante. Fusce eu vehicula velit, eu eleifend lorem. Aenean ullamcorper accumsan blandit. Fusce ultricies nulla ipsum, malesuada lacinia purus lacinia eu.",
 
- "Maecenas ac volutpat sapien, quis bibendum turpis. Donec varius ultrices ligula, eu venenatis quam fermentum id. Suspendisse cursus tristique bibendum. Pellentesque sit amet faucibus urna. Curabitur suscipit metus vitae auctor malesuada. Nulla facilisis suscipit tincidunt."]
+ "10 Maecenas ac volutpat sapien, quis bibendum turpis. Donec varius ultrices ligula, eu venenatis quam fermentum id. Suspendisse cursus tristique bibendum. Pellentesque sit amet faucibus urna. Curabitur suscipit metus vitae auctor malesuada. Nulla facilisis suscipit tincidunt."]
 
   for( var i=0; i<10; i++ ){
     curPosn = { lat: centralLatLng["lat"] + Math.random() * 0.01 - 0.005,
@@ -31,11 +31,24 @@ function dropMessagesStub( centralLatLng, map ){
       title: 'Click to view message'
     });
 
-    function myListener(j){
-        marker.addListener('click', function() {
-        console.log( messages[j] );
+    (function(j) {
+      marker.addListener('click', function() {
+
+        $('#view-message-modal .modal-title').html("GEO-MESSAGE TITLE");
+        $('#view-message-modal .author').html("by " + "USERNAME");
+        $('#view-message-modal .date').html("on " + "DATE");
+        $('#view-message-modal .modal-body .message').html(messages[j]);
+        $('#view-message-modal .modal-body .likes .like').html("LIKES");
+        $('#view-message-modal .modal-body .likes .dislike').html("DISLIKES");
+        positionToCityName(marker.position.lat(),marker.position.lng(), function(city) {
+          $('#view-message-modal .modal-body .location .city').html(city);
+        });
+
+        $('#view-message-modal').modal({
+          show: 'true'
+        });
+
       });
-    }
-    myListener(i);
+    })(i);
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,67 @@ router.get('/', function(req, res, next) {
 });
 
 // TEMP
+
+var YourMessages = [
+  {
+    createdOn: "Oct 20, 2016",
+    title: "I went to LONDON",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam ne.",
+    lat: 51.507351,
+    lng: -0.127758,
+    likes: 10,
+    dislikes: 40,
+  },
+  {
+    createdOn: "Nov 23, 2014",
+    title: "TOKYO!!!",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ne.",
+    lat: 35.689487,
+    lng: 139.691706,
+    likes: 100,
+    dislikes: 2,
+  },
+  {
+    createdOn: "Jan 14, 2015",
+    title: "Burnaby SUCKS",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ne.",
+    lat: 49.248809,
+    lng: -122.980510,
+    likes: 13,
+    dislikes: 6,
+  },
+];
+
+var readMessages = [
+  {
+    username: "Mclovin'",
+    createdOn: "Feb 23, 2014",
+    title: "My trip to Beijing",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ne.",
+    lat: 39.904211,
+    lng: 116.407395,
+    likes: 0,
+    dislikes: 1000,
+  },
+  {
+    username: "James Bond",
+    createdOn: "Jan 14, 2000",
+    title: "Concrete Jungle",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ne.",
+    lat: 40.712784,
+    lng: -74.005941,
+    likes: 14,
+    dislikes: 5,
+  },
+];
+
+var templateVars = {
+  "username": "JChow417",
+  "email": "JChow417@gmail.com",
+  'yourMessages': YourMessages,
+  'readMessages': readMessages
+};
+
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
@@ -14,7 +75,7 @@ router.get('/signup', function(req, res, next) {
   res.render('signup');
 });
 router.get('/profile', function(req, res, next) {
-  res.render('profile');
+  res.render('profile', templateVars);
 });
 
 module.exports = router;
