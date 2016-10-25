8ecc374488517728e56358dabbbead1cc0cc94ea
devin-web <devin.gir@gmail.com>
2016-10-25 19:27:16 +0000

    Grouped \'magic numbers\' into a single variable; got re-querying working for every 10m

--- public/map.js
+++ public/map.js
@@ -2,94 +2,112 @@ console.log('initMap declaration...');
 
 var coord;
 var centralPosnLatLng;
+var VISIBILITY_RADIUS = 400;
 
-function initMap() {
-  console.log( 'initializing the map...' );
+$( document ).ready( function() {
+  function initMap() {
+    console.log( 'initializing the map...' );
 
-  var myLatlng = {lat: 49.2827, lng: -123.1207};
+    var myLatlng = {lat: 49.2827, lng: -123.1207};
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: myLatlng
-  });
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: myLatlng
+    });
 
-  var options, id;
-  var mapCircle;
+    var options, id, originalCoord = {};
+    var mapCircle;
 
-  var firstCall =  true;
+    var firstCall =  true;
 
-  function success(pos){
+    function success(pos){
 
-    coord = { lat: pos.coords.latitude, lng: pos.coords.longitude } ;
-    centralPosnLatLng = new google.maps.LatLng( coord.lat, coord.lng );
+      coord = { lat: pos.coords.latitude, lng: pos.coords.longitude } ;
+      centralPosnLatLng = new google.maps.LatLng( coord.lat, coord.lng );
 
-    socket.emit('update position', coord);
+      if( firstCall ) {
+        originalCoord = new google.maps.LatLng( centralPosnLatLng.lat(), centralPosnLatLng.lng() );
+      }
 
-    console.log("success, current coord: ; ", coord );
+      var distanceTraveled = google.maps.geometry.spherical.computeDistanceBetween(
+        centralPosnLatLng, originalCoord );
 
-    if( firstCall ){
-      mapCircle = new google.maps.Circle({
-              strokeColor: '#0000DD',
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: '#0000DD',
-              fillOpacity: 0.35,
-              map: map,
-              center: coord,
-              radius: 400.0
-      });
-      renderMarkers( map );//myLatlng, map);
-      map.panTo( coord );
+      if( distanceTraveled > VISIBILITY_RADIUS/40 ){
+        console.log( "Distance Traveled triggered" );
+        originalCoord =  new google.maps.LatLng( centralPosnLatLng.lat(), centralPosnLatLng.lng() );
+        socket.emit('update position', coord);
+        renderMarkers( map );
+      }
+
+      console.log("success, current coord: ", coord );
+
+      if( firstCall ){
+        mapCircle = new google.maps.Circle({
+                strokeColor: '#0000DD',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#0000DD',
+                fillOpacity: 0.35,
+                map: map,
+                center: coord,
+                radius: VISIBILITY_RADIUS
+        });
+        socket.emit('update position', coord);
+        renderMarkers( map );//myLatlng, map);
+        map.panTo( coord );
+      }
+      mapCircle.setCenter( centralPosnLatLng );
+      firstCall = false;
     }
-    mapCircle.setCenter( centralPosnLatLng );
-    firstCall = false;
+
+    function error(err){
+      console.log("Error in geolocation: ", err);
+    }
+
+    options = {
+      enableHighAccuracy: false,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    id = navigator.geolocation.watchPosition(success, error, options);
+
+
+    // var marker = new google.maps.Marker({
+    //   position: myLatlng,
+    //   map: map,
+    //   title: 'Click to zoom'
+    // });
+
+    // map.addListener('center_changed', function() {
+    //   // 3 seconds after the center of the map has changed, pan back to the
+    //   // marker.
+    //   window.setTimeout(function() {
+    //     map.panTo(marker.getPosition());
+    //   }, 3000);
+    // });
+
+    var GeoMarker = new GeolocationMarker(map);
+    GeoMarker.setCircleOptions(
+      new google.maps.Circle({
+              strokeColor: '#DD0000',
+              strokeOpacity: 0.0,
+              strokeWeight: 2,
+              fillColor: '#DD0000',
+              fillOpacity: 0.0,
+              map: map,
+              center: GeoMarker.center,
+              radius: 40
+      })
+    );
+
+
+    // marker.addListener('click', function() {
+    //   map.setZoom(17);
+    //   map.setCenter(marker.getPosition());
+    // });
+
   }
-
-  function error(err){
-    console.log("Error in geolocation: ", err);
-  }
-
-  options = {
-    enableHighAccuracy: false,
-    timeout: 5000,
-    maximumAge: 0
-  };
-
-  id = navigator.geolocation.watchPosition(success, error, options);
-
-
-  // var marker = new google.maps.Marker({
-  //   position: myLatlng,
-  //   map: map,
-  //   title: 'Click to zoom'
-  // });
-
-  // map.addListener('center_changed', function() {
-  //   // 3 seconds after the center of the map has changed, pan back to the
-  //   // marker.
-  //   window.setTimeout(function() {
-  //     map.panTo(marker.getPosition());
-  //   }, 3000);
-  // });
-
-  var GeoMarker = new GeolocationMarker(map);
-  GeoMarker.setCircleOptions(
-    new google.maps.Circle({
-            strokeColor: '#DD0000',
-            strokeOpacity: 0.0,
-            strokeWeight: 2,
-            fillColor: '#DD0000',
-            fillOpacity: 0.0,
-            map: map,
-            center: GeoMarker.center,
-            radius: 40
-    })
-  );
-
-
-  // marker.addListener('click', function() {
-  //   map.setZoom(17);
-  //   map.setCenter(marker.getPosition());
-  // });
-
-}
+  //setTimeout(initMap, 3000);
+  initMap();
+});
