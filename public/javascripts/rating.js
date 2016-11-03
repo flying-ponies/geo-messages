f34603e3cf9e1f11a98ed144165d6b46e1b394f6
John Chow <JChow417@gmail.com>
2016-11-03 00:45:34 +0000

    removed front end console logs

--- public/javascripts/rating.js
+++ public/javascripts/rating.js
@@ -1,15 +1,12 @@
 function clickedThumbsUp(){
-  console.log( "Thumbs up! id:", this.dataset.messageId );
   socket.emit( "message liked", this.dataset.messageId );
 }
 
 function clickedThumbsDown(){
-  console.log( "Thumbs down! id:", this.dataset.messageId );
   socket.emit( "message disliked", this.dataset.messageId );
 }
 
 function registerRatingEventHooks(){
-  console.log( "Registering Event Handlers" );
 
   $("#myTabContent").on("click", "span.glyphicon.glyphicon-thumbs-up", clickedThumbsUp );
   $("#myTabContent").on("click", "span.glyphicon.glyphicon-thumbs-down", clickedThumbsDown );
@@ -17,10 +14,8 @@ function registerRatingEventHooks(){
   $("#view-message-modal").on("click", "span.glyphicon.glyphicon-thumbs-down", clickedThumbsDown );
 
   socket.on('message liked success', function( messageId ){
-    console.log("message liked success");
     var $thumbs = $('span.glyphicon.glyphicon-thumbs-up[data-message-id=' + messageId + ']');
     if ($thumbs.length === 1) {
-      console.log("liked matched");
       var likes = $thumbs.parent().find("span.like");
       var dislikes = $thumbs.parent().find("span.dislike");
       likes.text( Number(likes.text()) + 1 );
@@ -29,11 +24,9 @@ function registerRatingEventHooks(){
   });
 
   socket.on('message disliked success', function( messageId ){
-    console.log("message disliked success");
 
     var $thumbs = $('span.glyphicon.glyphicon-thumbs-up[data-message-id=' + messageId + ']');
     if ($thumbs.length === 1) {
-      console.log("disliked matched");
       var likes = $thumbs.parent().find("span.like");
       var dislikes = $thumbs.parent().find("span.dislike");
       likes.text( Number(likes.text()) - 1 );
